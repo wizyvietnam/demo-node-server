8a3ae66a03f1ef1ca81594a910dcb884515ba4cf
HoanDo <hoan.yesi1@gmail.com>
2019-01-02 01:24:49 +0700

    Fix some eslint issue

--- env.js
+++ env.js
@@ -1,5 +1,4 @@
 import fs from 'fs';
-import path from 'path';
 import dotenv from 'dotenv';
 
 let NODE_ENV = process.env.NODE_ENV;
@@ -8,18 +7,22 @@ if (!NODE_ENV) {
   process.env.NODE_ENV = 'development';
 }
 
-let dotenvFiles = [
-  `${process.cwd()}/.env.${NODE_ENV}`,
-].filter(Boolean);
+let dotenvFiles = [`${process.cwd()}/.env.${NODE_ENV}`].filter(Boolean);
 
 dotenvFiles.forEach(dotenvFile => {
   if (fs.existsSync(dotenvFile)) {
     dotenv.config({
-      path: dotenvFile,
-    })
+      path: dotenvFile
+    });
   }
 });
 
-if(!process.env.END_POINT) {throw new Error('process.env.END_POINT is missing!')}
-if(!process.env.PORT) {throw new Error('process.env.PORT is missing!')}
-if(!process.env.MONGODB_URI) {throw new Error('process.env.MONGODB_URI is missing!')}
+if (!process.env.END_POINT) {
+  throw new Error('process.env.END_POINT is missing!');
+}
+if (!process.env.PORT) {
+  throw new Error('process.env.PORT is missing!');
+}
+if (!process.env.MONGODB_URI) {
+  throw new Error('process.env.MONGODB_URI is missing!');
+}
