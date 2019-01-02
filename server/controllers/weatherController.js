5b631b148612aeaae92b252552ae09a488e83d43
HoanDo <hoan.yesi1@gmail.com>
2019-01-02 20:27:25 +0700

    Update create and delete function

--- server/controllers/weatherController.js
+++ server/controllers/weatherController.js
@@ -7,7 +7,22 @@ export const weatherController = {
   },
   async getWeatherByID(req, res) {
     const { extID } = req.params;
-    const weather = await WeatherServices().getOne({ extId: Number.parseInt(extID) });
+    const weather = await WeatherServices().getOne({
+      extId: Number.parseInt(extID)
+    });
     res.ok({ success: true, result: weather });
+  },
+  async createNewWeatherInfo(req, res) {
+    const weatherInfo = req.body.weather;
+    await WeatherServices().create(weatherInfo);
+    res.ok({ success: true, result: weatherInfo });
+  },
+  async updateWeather(req, res) {
+    
+  },
+  async deleteWeatherByID(req, res) {
+    const weatherInfo = req.body.weather;
+    await WeatherServices().delete(weatherInfo);
+    res.ok({ success: true, result: extID });
   }
 };
--- server/routes.js
+++ server/routes.js
@@ -6,5 +6,7 @@ const routes = new Router();
 
 routes.get('/weathers', weatherController.getAllWeather);
 routes.get('/weather/:extID', weatherController.getWeatherByID);
+routes.post('/weather', weatherController.createNewWeatherInfo);
+routes.delete('/weather/:extID', weatherController.deleteWeatherByID);
 
 export default routes;
