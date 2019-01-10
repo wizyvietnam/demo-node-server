256d0fc6f56f6f261bc891742d2dcf5a08deb933
HoanDo <hoan.yesi1@gmail.com>
2019-01-10 22:50:10 +0700

    Update delete function

--- server/services/weatherService.js
+++ server/services/weatherService.js
@@ -1,5 +1,12 @@
 import { BaseServices } from './shared/baseServices';
 
 export const WeatherServices = () => {
-  return Object.assign({}, BaseServices('weather'));
+  return Object.assign({}, BaseServices('weather'), {
+    delete: ({ id }) => {
+      return base.getById(id).then(freelancer => {
+        if (!freelancer) throw new Error('freelancer not found');
+        return base.update(id, { isActive: false });
+      });
+    }
+  });
 };
